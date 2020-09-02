4d52d91939bee4c95063f11a54a5acdfe3c32783
Filipe Bezerra <filipebzerra@gmail.com>
2020-09-02 14:53:07 -0300

    Navigation to the game destination

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -1,23 +1,32 @@
 package com.example.android.navigation
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation.createNavigateOnClickListener
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 class TitleFragment : Fragment() {
 
+    private lateinit var binding: FragmentTitleBinding
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        val binding = DataBindingUtil.inflate<FragmentTitleBinding>(
+        binding = DataBindingUtil.inflate(
                 layoutInflater,
                 R.layout.fragment_title,
                 container,
                 false
         )
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.playButton.setOnClickListener(
+                createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment))
     }
 }
